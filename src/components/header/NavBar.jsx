29d5d5b2f4f4ed03f1cdbb46e112060e391c1e71
FRI3NDS Ấn <19521187@gmail.com>
2022-06-08 20:37:14 +0700

    change size logo

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -7,9 +7,9 @@ import Section from "../../wrapper/Header/DropdownSection";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row justify-between px-10 py-4 items-center fixed top-0 w-full z-50 shadow-sm bg-white">
+    <div className="flex flex-row justify-between px-10 py-5 items-center fixed top-0 w-full z-50 shadow-sm bg-white">
       <Link to={"/"}>
-        <img src={Logo} alt="logo" className="w-[12vw] h-[10vh]" />
+        <img src={Logo} alt="logo" className="w-[120px] h-auto" />
       </Link>
       <div className="flex flex-row gap-5">
         {SectionData.map((section, index) => (
