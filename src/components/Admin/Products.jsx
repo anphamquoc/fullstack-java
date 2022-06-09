8d4a8a01fbfda9e6471bd1a01058209e4dad2eb8
tdb <tranducbao192@gmail.com>
2022-06-09 10:44:27 +0700

    themsanpham

--- src/components/Admin/Products.jsx
+++ src/components/Admin/Products.jsx
@@ -163,7 +163,7 @@ const Products = () => {
                 />
                 <label htmlFor="upload-profile-image">
                   <Button variant="contained" color="primary" component="span">
-                    Change Image
+                    Thay đổi hình ảnh
                   </Button>
                 </label>
                 <TextField
@@ -203,7 +203,7 @@ const Products = () => {
                   className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
                   data-bs-dismiss="modal"
                 >
-                  Close
+                  Đóng
                 </button>
                 <button
                   type="button"
@@ -213,7 +213,7 @@ const Products = () => {
                   {loading ? (
                     <i class="fad fa-spinner-third fa-spin"></i>
                   ) : (
-                    "Save change"
+                    "Thêm sản phẩm"
                   )}
                 </button>
               </div>
